c2b2341f0cabc7ae04988e58322ead3e91ab234d
hanaZRasheed <alrasheedhana@gmail.com>
2022-05-25 09:53:49 +0300

    .

--- evebtdriven1/Demo2.java
+++
@@ -1,9 +0,0 @@
-package evebtdriven1;
-
-public class Demo2 {
-public static void main(String[] args) {
-	MyFrame frame1=new MyFrame();
-	MyFrame frame2=new MyFrame();
-	MyFrame frame3=new MyFrame();
-}
-}
